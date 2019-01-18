1d496748b6e5e15e6f63db3ecc178a83de23364a
boudabou <hani.boudabous@epfl.ch>
2019-01-18 22:57:26 +0100

    Updates

---
+++ precache-manifest.428124dac60a065c527ed36fb56c0b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dictionary-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f2b796ab099b1c9446b9",
+    "url": "/dictionary-app/static/js/runtime~main.f2b796ab.js"
+  },
+  {
+    "revision": "8260d71d6bb7ddae7015",
+    "url": "/dictionary-app/static/js/main.8260d71d.chunk.js"
+  },
+  {
+    "revision": "6dc92c01491d4233bcfc",
+    "url": "/dictionary-app/static/js/1.6dc92c01.chunk.js"
+  },
+  {
+    "revision": "8260d71d6bb7ddae7015",
+    "url": "/dictionary-app/static/css/main.ba12b43c.chunk.css"
+  },
+  {
+    "revision": "6dc92c01491d4233bcfc",
+    "url": "/dictionary-app/static/css/1.66f54fff.chunk.css"
+  },
+  {
+    "revision": "63c5b276828009c330bd9be3b37f99ec",
+    "url": "/dictionary-app/index.html"
+  }
+];
